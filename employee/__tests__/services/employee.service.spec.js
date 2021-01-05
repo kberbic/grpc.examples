a3ca89f919a7a3b55b40b1dcb6aa425be6fba0bc
Kenan Berbic <kberbic@Kenans-MacBook-Pro.local>
2021-01-05 14:36:52 +0100

    Add statistic ms and create connections between ms-s, and fixed lint issues

--- employee/__tests__/services/employee.service.spec.js
+++ employee/__tests__/services/employee.service.spec.js
@@ -3,7 +3,7 @@
 import EmployeeService from '../../services/employee.service.js';
 
 describe('employee.service.js', () => {
-    it('If EmployeeService is initialized, response is EmployeeService', () => {
-        expect(EmployeeService).toHaveProperty("proto");
-    });
+  it('If EmployeeService is initialized, response is EmployeeService', () => {
+    expect(EmployeeService).toHaveProperty('proto');
+  });
 });
--- employee/models/index.js
+++ employee/models/index.js
@@ -4,20 +4,21 @@ import mongoose from 'mongoose';
 
 const PATH = `${path.resolve()}/models`;
 const loadModels = async () => fs.readdirSync(PATH)
-    .reduce(async (promise, file) => promise.then(async (models) => {
-        const fullPath = path.join(PATH, file);
-        if (file.endsWith('.model.js')) {
+  .reduce(async (promise, file) => promise.then(async (models) => {
+    const fullPath = path.join(PATH, file);
+    if (file.endsWith('.model.js')) {
             const Model = await import(fullPath); // eslint-disable-line
-            models[Model.default.modelName] = Model.default;
-        }
-        return models;
-    }), Promise.resolve({}));
+      models[Model.default.modelName] = Model.default;
+    }
+    return models;
+  }), Promise.resolve({}));
 
 const models = await loadModels();
 models.lib = mongoose;
 models.connect = mongoose.connect;
 models.init = async () => mongoose.connect(
-    process.env.DATABASE_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true });
+  process.env.DATABASE_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+);
 
 export default models;
