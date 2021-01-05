6a25ed34eee1e41c4e8fac6b4f7fc188c9e8d2ad
Kenan Berbic <kberbic@Kenans-MacBook-Pro.local>
2021-01-05 11:42:38 +0100

    Init

---
+++ company/errors/invalidarguments.error.js
@@ -0,0 +1,13 @@
+import ServiceError from './service.error.js';
+
+export default class InvalidArgumentsError extends ServiceError {
+    constructor(message, fields) {
+        super(ServiceError.GRPC_CODES.INVALID_ARGUMENT, message);
+        if (fields)
+            this.metadata
+                .internalRepr
+                .set("fields", [JSON.stringify(fields
+                    .inner
+                    .map(field => ({[field.path]: field.errors})))])
+    }
+}
